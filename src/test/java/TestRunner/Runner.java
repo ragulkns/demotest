b9ad1a531c46a1d06ae4ff5c78471aa85406d31b
ragulkns <58589748+ragulkns@users.noreply.github.com>
2025-01-13 16:50:00 +0530

    Update Runner.java

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -15,7 +15,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
    
-                features="src/test/resources",
+                features={"src/test/resources"},
                 glue={"stepDefinition"},
                 dryRun = false,
                 //tags = "@TC4",
